b6e85b05b3b770ab3a56c114f7b0c4fd49088c6c
JungManKyung <mk.jung01@gretail.com>
2023-12-15 16:47:05 +0900

    treeshaking

--- plugins/antd.ts
+++ plugins/antd.ts
@@ -1,7 +1,41 @@
 import { defineNuxtPlugin } from "#app";
 import "ant-design-vue/dist/reset.css";
-import Antd from "ant-design-vue";
+// import Antd from "ant-design-vue";
+
+import {
+  Button,
+  Card,
+  CardMeta,
+  Input,
+  Layout,
+  List,
+  ListItem,
+  Radio,
+  RadioGroup,
+  Spin,
+  Row,
+  Col,
+  Popover,
+  Textarea,
+  Tag,
+  Select,
+} from "ant-design-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Antd);
+  nuxtApp.vueApp.use(Button);
+  nuxtApp.vueApp.use(Card);
+  nuxtApp.vueApp.use(CardMeta);
+  nuxtApp.vueApp.use(Input);
+  nuxtApp.vueApp.use(Layout);
+  nuxtApp.vueApp.use(List);
+  nuxtApp.vueApp.use(ListItem);
+  nuxtApp.vueApp.use(Radio);
+  nuxtApp.vueApp.use(RadioGroup);
+  nuxtApp.vueApp.use(Spin);
+  nuxtApp.vueApp.use(Row);
+  nuxtApp.vueApp.use(Col);
+  nuxtApp.vueApp.use(Popover);
+  nuxtApp.vueApp.use(Textarea);
+  nuxtApp.vueApp.use(Tag);
+  nuxtApp.vueApp.use(Select);
 });
